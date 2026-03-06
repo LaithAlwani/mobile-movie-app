0a136cff96457e1098d9900fd85992d610d8efd5
laith <laithalwani@gmail.com>
2026-03-06 13:25:46 -0500

    hide status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from "expo-router";
 import "./globals.css";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { StatusBar } from "react-native";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!);
 
 export default function RootLayout() {
   return (
     <ConvexProvider client={convex}>
+      <StatusBar hidden={true} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
